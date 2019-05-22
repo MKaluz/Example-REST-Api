402ae3fd3ebff3cd4173248d63323bbd9574491c
MKaluz <michal.kaluza9@gmail.com>
2019-05-22 17:25:27 +0200

    Initial

---
+++ REST.Data/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using REST.Data.Models;
+
+namespace REST.Data.Context
+{
+   public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+        :base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
